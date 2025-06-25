5397977ba8f7d87473a96170f7d7620f8f0864cc
sakky sak <sgiri331@student.fuhsd.org>
2025-06-24 17:46:10 -0700

    homework

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,1 +1,44 @@
 // Homework for Week 2
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.Slot0Configs;
+import com.ctre.phoenix6.controls.PositionVoltage;
+
+public class ArmSubsystem extends SubsystemBase {
+    private static final double kRotationsPerRadian = 10.0;
+    private final TalonFX motor;
+    private double targetRadians;
+    private final PositionVoltage positionRequest;
+
+    public ArmSubsystem() {
+        motor = new TalonFX(2);
+        Slot0Configs slot0 = new Slot0Configs();
+        slot0.kP = 2.0;
+        slot0.kI = 0.0;
+        slot0.kD = 0.1;
+        motor.getConfigurator().apply(slot0);
+        positionRequest = new PositionVoltage(0).withSlot(0);
+    }
+
+    public void setAngle(double radians) {
+        targetRadians = radians;
+        motor.setControl(positionRequest.withPosition(radians * kRotationsPerRadian));
+    }
+
+    public double getAngle() {
+        return motor.getRotorPosition().getValueAsDouble() / kRotationsPerRadian;
+    }
+
+    public void zeroEncoder() {
+        motor.setPosition(0.0);
+    }
+
+    public boolean isAtTargetAngle(double toleranceRadians) {
+        return Math.abs(getAngle() - targetRadians) <= toleranceRadians;
+    }
+    
+    public void stop() {
+        setAngle(getAngle());
+    }
+}
