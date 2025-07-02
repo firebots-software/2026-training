8599343ac989e548b02aaa8be3dd03fb28d95025
73anthonyL <73anthony.lu@gmail.com>
2025-07-01 21:30:35 -0700

    first commit

---
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -0,0 +1,14 @@
+// Instructions:
+// 1. create a branch off of this branch and title it firstname-lastname-week-4
+// 2. you are responsible for instantiating the two motors for this subsystem
+// 3. you are responsible for applying stator and supply current limits
+//    for each of the motors (recommended 65 Amp stator current limits & 30 Amp limit for supply)
+// 4. use the appropriate control mode for running the Tank Drive (and leave a comment for why you are using it)
+// Hint: besides defining the motors in the constructor, the main method that your Tank Drive
+// needs takes in input to drive the left and right motors each with a control request
+
+package frc.robot.subsystems;
+
+public class TankDriveSubsystem {
+    
+}
