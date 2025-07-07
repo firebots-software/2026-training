eec608d8f93702bf93a9446da5a1d6f1ae572921
arj2cool <arjunv.lzp@gmail.com>
2025-07-07 16:58:02 -0700

    did my hw like a BOSS

--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -12,7 +12,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -21,8 +21,8 @@ public class TankDriveSubsystem extends SubsystemBase {
     private final TalonFX motorLeft;
     private final TalonFX motorRight;
 
-    private final PositionVoltage positionRequestLeft;
-    private final PositionVoltage positionRequestRight;
+    private final DutyCycleOut dutyCycleRequestLeft = new DutyCycleOut(0.0);
+    private final DutyCycleOut dutyCycleRequestRight = new DutyCycleOut(0.0);
 
     public TankDriveSubsystem() {
         motorLeft = new TalonFX(0);
@@ -33,15 +33,7 @@ public class TankDriveSubsystem extends SubsystemBase {
         slot0.kI = Constants.TankDrive.PIDValues.kI;
         slot0.kD = Constants.TankDrive.PIDValues.kD;
         motorLeft.getConfigurator().apply(slot0);
-
-        Slot0Configs slot1 = new Slot0Configs();
-        slot1.kP = Constants.TankDrive.PIDValues.kP;
-        slot1.kI = Constants.TankDrive.PIDValues.kI;
-        slot1.kD = Constants.TankDrive.PIDValues.kD;
-        motorRight.getConfigurator().apply(slot1);
-
-        positionRequestLeft = new PositionVoltage(0).withSlot(0);
-        positionRequestRight = new PositionVoltage(0).withSlot(1);
+        motorRight.getConfigurator().apply(slot0);
 
         CurrentLimitsConfigs clcTankDrive = new CurrentLimitsConfigs().withStatorCurrentLimitEnable(true).withStatorCurrentLimit(Constants.TankDrive.CurrentLimits.STATOR_CURRENT_LIMIT_AMPS).withSupplyCurrentLimitEnable(true).withSupplyCurrentLimit(Constants.TankDrive.CurrentLimits.SUPPLY_CURRENT_LIMIT_AMPS);
         TalonFXConfigurator MasterConfiguraterTankDriveLeft = motorLeft.getConfigurator();
@@ -50,4 +42,8 @@ public class TankDriveSubsystem extends SubsystemBase {
         MasterConfiguraterTankDriveRight.apply(clcTankDrive);
     }
 
+    public void drive(double speedLeft, double speedRight) {
+        motorLeft.setControl(dutyCycleRequestLeft.withOutput(speedLeft));
+        motorRight.setControl(dutyCycleRequestRight.withOutput(speedRight));
+    }
 }
