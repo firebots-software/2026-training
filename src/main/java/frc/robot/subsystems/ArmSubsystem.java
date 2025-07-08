5b453a0f272906f70aca8e0d7bf20a30150f34a7
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-07-07 18:06:18 -0700

    add arm subsystem code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,62 +1,47 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.PositionVoltage;
 
 public class ArmSubsystem extends SubsystemBase {
-  private final TalonFX motor;
-  private double targetRadians;
+  // 10 rotations of the arm motor = 1 radian of movement
+  // or about 0.1745 degrees per rotation
+  private static final int rotationsPerRadian = 10;
+  private static final double rotationsPerDegree = 0.1745;
 
+  private final TalonFX motor;
+  // Set both so that both radians and degrees can be set
+  private double targetRadians;
+  private double targetDegrees;
   private final PositionVoltage positionRequest;
 
   public ArmSubsystem() {
-    targetRadians = (3.14 / 2d);
-
     motor = new TalonFX(1);
-    Slot0Configs slot0 = new Slot0Configs();
-    slot0.kP = Constants.ArmConstants.kP;
-    slot0.kI = Constants.ArmConstants.kI;
-    slot0.kD = Constants.ArmConstants.kD;
-    motor.getConfigurator().apply(slot0);
 
     positionRequest = new PositionVoltage(0).withSlot(0);
   }
 
-  public void setAngle(double armRadians) {
-    targetRadians = armRadians;
+  // Allow choice between setting radians or degrees
+  public void setAngle(double angle, boolean radians) {
+    if (radians) {
+      targetRadians = angle;
+      motor.setControl(positionRequest.withPosition(angle * rotationsPerRadian));
+    } else {
+      targetDegrees = angle;
+      motor.setControl(positionRequest.withPosition(angle * rotationsPerDegree));
+    }
   }
 
-  public double getAngle() {
-    return motor.getRotorPosition().getValueAsDouble() / Constants.ArmConstants.rotationsPerRadian;
-  }
-
-  public void zeroEncoder() {
-    motor.setPosition(0.0);
-  }
-
-  public boolean isAtTargetAngle(double toleranceRadians) {
-    return Math.abs(getAngle() - targetRadians) <= toleranceRadians;
+  public double getAngle(boolean radians) {
+    if (radians) {
+      return motor.getRotorPosition().getValueAsDouble() / rotationsPerRadian;
+    } else {
+      return motor.getRotorPosition().getValueAsDouble() / rotationsPerDegree;
+    }
   }
 
   public void stop() {
-    setAngle(getAngle());
-  }
-
-  @Override
-  public void periodic() {
-    motor.setControl(positionRequest.withPosition(targetRadians * Constants.ArmConstants.rotationsPerRadian));
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    setAngle(getAngle(true), true);
   }
 }
