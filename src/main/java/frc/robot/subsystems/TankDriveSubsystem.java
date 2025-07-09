003a40857599c11fa0556ee28623d9a95a1cc979
Dan36252 <danielabkairov@gmail.com>
2025-07-08 21:26:53 -0700

    Implemented the TankDriveSubsystem

--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -9,6 +9,51 @@
 
 package frc.robot.subsystems;
 
-public class TankDriveSubsystem {
-    
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TankDriveSubsystem extends SubsystemBase {
+    // Instantiating the two motors of the Tank Drive Subsystem
+    private final TalonFX leftMotor = new TalonFX(0);
+    private final TalonFX rightMotor = new TalonFX(1);
+
+    // Using VoltageOut instead of DutyCycle control request because allows for precise voltage control instead of percent of total available current, which is unstable
+    private VoltageOut voltageRequestR, voltageRequestL;
+
+    public TankDriveSubsystem() {
+        // Initialize Voltage Out Requests
+        voltageRequestR = new VoltageOut(0.0);
+        voltageRequestL = new VoltageOut(0.0);
+
+        // Apply Stator and Supply current limits to the motors
+        TalonFXConfigurator rightMotorConfigurator = rightMotor.getConfigurator();
+        TalonFXConfigurator leftMotorConfigurator = leftMotor.getConfigurator();
+        
+        CurrentLimitsConfigs currentLimitConfigs =
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true)
+                .withStatorCurrentLimit(65)
+                .withSupplyCurrentLimitEnable(true)
+                .withSupplyCurrentLimit(30);
+        
+        rightMotorConfigurator.apply(currentLimitConfigs);
+        leftMotorConfigurator.apply(currentLimitConfigs);
+    }
+
+    // Main method for controling the Tank Drivebase
+    public void RunMotorsAtVoltages(double leftVoltage, double rightVoltage) {
+        leftMotor.setControl(voltageRequestL.withOutput(leftVoltage));
+        rightMotor.setControl(voltageRequestR.withOutput(rightVoltage));
+    }
+
+    // Stop all motors
+    public void StopMotors() {
+        leftMotor.stopMotor();
+        rightMotor.stopMotor();
+    }
 }
