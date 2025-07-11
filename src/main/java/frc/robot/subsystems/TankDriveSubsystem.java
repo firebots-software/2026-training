7bc95b0660559022f9481f9188d945b9f7ca56e6
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-07-11 15:40:38 -0700

    Tank drive subystem code

--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -1,5 +1,5 @@
 // Instructions:
-// 1. create a branch off of this branch and title it firstname-lastname-week-4
+// x. create a branch off of this branch and title it firstname-lastname-week-4
 // 2. you are responsible for instantiating the two motors for this subsystem
 // 3. you are responsible for applying stator and supply current limits
 //    for each of the motors (recommended 65 Amp stator current limits & 30 Amp limit for supply)
@@ -9,6 +9,32 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX; // motor
+
 public class TankDriveSubsystem {
-    
+    private final DutyCycleOut dutyCycleRequest = new DutyCycleOut(0.0);
+
+    private final TalonFX motorLeft;
+    private final TalonFX motorRight;
+
+    public TankDriveSubsystem() {
+        motorLeft = new TalonFX(1);
+        motorRight = new TalonFX(2);
+
+        CurrentLimitsConfigs clcDrive = new CurrentLimitsConfigs()
+            .withStatorCurrentLimitEnable(true)
+            .withStatorCurrentLimit(65)
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(30);
+        
+        motorLeft.getConfigurator().apply(clcDrive);
+        motorRight.getConfigurator().apply(clcDrive);
+    }
+
+    public void drive(double leftMotorValue, double rightMotorValue) {
+        motorLeft.setControl(dutyCycleRequest.withOutput(leftMotorValue));
+        motorRight.setControl(dutyCycleRequest.withOutput(rightMotorValue));
+    }
 }
